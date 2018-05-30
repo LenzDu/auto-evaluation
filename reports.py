76f3befab9e6f26a942c908223bfe6c27d53d6a4
cerhai <449292359@qq.com>
2018-05-29 17:44:51 -0700

    Add reports.py

--- reports.py
+++ reports.py
@@ -1,1 +1,8 @@
 """Generating Reports"""
+import pandas as pd
+import auto_evaluation.templates as templates
+
+
+def to_html(sample, stats):
+    
+    return templates.templates('base').render({})
